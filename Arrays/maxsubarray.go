ff0c2502cdb9c02398ce67346e93afdb23a47708
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2022-07-09 19:17:29 -0400

    Sall tweak

--- Arrays/maxsubarray.go
+++ Arrays/maxsubarray.go
@@ -1,7 +1,5 @@
 package main
 
-// Write a function that takes an array of integers and returns the largest subarray sum.
-// we should be able to handle arrays containing negative elements
 func maxSubArray(nums []int) int {
 	if len(nums) == 0 {
 		return 0
